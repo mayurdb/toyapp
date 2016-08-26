ee38e885fb26235848b52275b1a81910bfb8d63b
mayurdb <mayurdb31@gmail.com>
2016-08-27 01:34:25 +0530

    Second

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-  	render html: "hello world!"
+  	render html: "hello world, how u doing!"
+  end
+  def index
+  	render html:"this is index"
   end
 end
